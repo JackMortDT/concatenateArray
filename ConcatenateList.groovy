7cc7c41ff087acd140d344797f54aac088ae358e
LuisSas <luis@makingdevs.com>
2018-06-22 16:30:01 -0500

    Agregando metodos de concatenación de arrays y su respectiva prueba

---
+++ ConcatenateList.groovy
@@ -0,0 +1,59 @@
+class ConcatenateList{
+  def add(List data){
+    List listOfUniques = getUniqueValues(data)
+    Integer counter = 0
+    List<String> nullValue = testVoidValue(data)
+    List evaluationList = []
+    data.each{ row ->
+      if(row == listOfUniques){
+        counter++
+      }
+      else{
+        listOfUniques.each{ unique->
+          row.each{ cell ->
+            if (cell == unique){
+              evaluationList << cell
+            } else if (cell == []){
+              evaluationList << []
+            }
+          }
+        }
+      }
+    }
+    if(counter == data.size()){
+      listOfUniques
+    } else if (nullValue.contains("null value")){
+      [[]]
+    } else {
+      evaluationList
+    }
+  }
+
+  List getUniqueValues(List data){
+    List listOfUniques = []
+    data.each{ evaluation ->
+      evaluation.each{
+        Map nameOfN = [n:it.n]
+        listOfUniques << nameOfN
+      }
+    }
+    listOfUniques.unique()
+  }
+
+  List<String> testVoidValue(List data){
+    List voidList = []
+    List itHasVoidValues = []
+    data.each{ row ->      
+      row.each{ cell ->
+        if(cell == voidList){
+          itHasVoidValues << "null value"
+        }
+      }
+    }
+    itHasVoidValues
+  }
+
+
+
+
+}
---
+++ ConcatenateListSpec.groovy
@@ -0,0 +1,94 @@
+@Grab(group='org.spockframework', module='spock-core', version='1.1-groovy-2.4', scope='test')
+
+import spock.lang.Specification
+
+class ConcatenateListSpec extends Specification{
+  ConcatenateList cl = new ConcatenateList()
+
+  void "first case of data"(){
+    given:
+      List data = [[[n:1], [n:2]],
+                   [[n:1], [n:2]],
+                   [[n:1], [n:2]]]
+    when:
+      def result = cl.add(data)
+    then:
+      result == [[n:1],[n:2]]
+  }
+
+  void "second case of data"(){
+    given:
+      List data = [[[n:1], [n:2]],
+                   [[n:1], [n:2]],
+                   [[n:1]]]
+    when:
+      def result = cl.add(data)
+    then:
+      result == [[n:1]]
+  }
+
+  void "third case of data"(){
+    given:
+      List data = [[[n:1], [n:2]],
+                   [[n:1], [n:2]],
+                   [[n:2]]]
+    when:
+      def result = cl.add(data)
+    then:
+      result == [[n:2]]
+  }
+
+  void "fourth case of data"(){
+    given:
+      List data = [[[n:1], [n:2], [n:3]],
+                   [[n:1], [n:2]]]
+    when:
+      def result = cl.add(data)
+    then:
+      result == [[n:1], [n:2]]
+  }
+
+  void "fifth case of data"(){
+    given:
+      List data = [[[n:1], [n:2], [n:3]], 
+                   [[n:1], [n:2], [n:3]]]
+    when:
+      def result = cl.add(data)
+    then:
+      result == [[n:1], [n:2], [n:3]]
+  }
+
+  void "sixth case of data"(){
+    given:
+      List data = [[[n:1], [n:2], [n:3]],
+                   [[n:1], [n:2], [n:3]],
+                   [[n:3]]]
+    when:
+      def result = cl.add(data)
+    then:
+      result == [[n:3]]
+  }
+
+  void "seventh case of data"(){
+    given:
+      List data = [[[n:1], [n:2], [n:3]],
+                   [[n:1], [n:2], [n:3]],
+                   [[]]]
+    when:
+      def result = cl.add(data)
+    then:
+      result == [[]]
+  }
+
+  void "seventh case of data"(){
+    given:
+      List data = [[[n:1], [n:2], [n:3]],
+                   [[n:1], [n:2]],
+                   [[n:1]]]
+    when:
+      def result = cl.add(data)
+    then:
+      result == [[n:1]]
+  }
+  
+}
